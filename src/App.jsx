06c21fb7814e529a9529ab8fc944e8af7e890b8a
Nani-p-max <nanipodatharapu@gmal.com>
2026-02-07 09:31:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const API_key='300029031a5b17d36ac7b104874683a1';
+
+export default function App() {
+  const[city,setCity]=useState("");
+  const[weather,setWeather]=useState(null);
+  const[error,setError]=useState("");
+
+  const getWeather=async()=>{
+    if(!city){
+      setError("Please enter a city name");
+      return;
+    }
+
+    try{
+      setError("");
+      const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
+
+    if(!response.ok){
+      setError("City not found");
+      return;
+    }
+    const data=await response.json();
+    setWeather(data);
+    
+    }
+    catch(error){
+      setWeather(null);
+      setError(error.message);
+    }
+  };
+  return (
+    <div className='App'>
+      <div className='header'>
+        <h1>Weather App</h1>
+      </div>
+      <div className='section'>
+        <div className='search'>
+          <input type='text' placeholder='Enter city name' value={city} onChange={(e)=>setCity(e.target.value)} required/>
+          <button type='submit' placeholder='submit' onClick={getWeather}>Search</button>
+        </div>
+        {error && <p className='error'>{error}</p>}
+        {weather && (
+          <div className='weather-card'>
+            <h2>{weather.name}</h2>
+            <p>Temperature: {weather.main.temp}°C</p>
+            <p>Humidity: {weather.main.humidity}%</p>
+            <p>Weather: {weather.weather[0].description}</p>
+            <p>Wind Speed: {weather.wind.speed} m/s</p>
+          </div>
+        )}
+      </div>
+      <div className='footer'>
+        <p>copyright reserved @ Narendra 2026 . All rights reserved</p>
+      </div>
+    </div>
+  )
+}
+
+
